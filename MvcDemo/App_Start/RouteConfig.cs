cf51d9b0560ff738c52fc83aae5d43a8b732d08d
neil.tao <neil.tao@titansoft>
2018-10-25 23:44:24 +0800

    add complexRoute

--- MvcDemo/App_Start/RouteConfig.cs
+++ MvcDemo/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MvcDemo
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "complexRoute",
+                url: "{id}/{name}",
+                defaults: new { controller = "Route", action = "Index"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ MvcDemo/Controllers/RouteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class RouteController : Controller
+    {
+        // GET: Route
+        public ActionResult Index( string id, string name)
+        {
+            return View();
+        }
+    }
+}
